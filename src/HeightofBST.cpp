02683c1785f42630cc8d531f9cc171b6be8a886c
vivekananda-reddy <vivekananda-reddy@missionrnd.com>
2016-04-16 22:55:03 +0530

    Submit Commit

--- src/HeightofBST.cpp
+++ src/HeightofBST.cpp
@@ -40,17 +40,58 @@ struct node{
 	struct node *right;
 };
 
-
+int sum_recurr(struct node*, int);
+int height_recur(struct node*);
 int get_height(struct node *root){
-
-	return 0;
+	
+	if (root == NULL)
+	{
+		return -1;
+	}
+	
+	return (height_recur(root));
 }
-
+int height_recur(struct node*root)
+{
+	int lefttree, righttree;
+	if (root == NULL)
+	{
+		return 0;
+	}
+	lefttree = height_recur(root->left);
+	righttree =height_recur(root->right);
+	if (lefttree > righttree)
+	{
+		return lefttree + 1;
+	}
+	return righttree + 1;
+}
 int get_left_subtree_sum(struct node *root){
-	return 0;
+	if (root == NULL)
+	{
+		return -1;
+	}
+	return(sum_recurr(root->left,0));
+
+}
+int sum_recurr(struct node *root,int sum)
+{
+
+	if (root == NULL)
+	{
+		return 0;
+	}
+	sum=sum+sum_recurr(root->left,sum);
+	sum=sum+sum_recurr(root->right,sum);
+	sum = sum + root->data;
+	return sum;
 }
 
 int get_right_subtree_sum(struct node *root){
-	return 0;
+	if (root == NULL)
+	{
+			return -1;
+	}
+	return(sum_recurr(root->right, 0));
 }
 
