2cd5babc560d75299a820de7f2577571ece71803
vivekananda-reddy <vivekananda-reddy@missionrnd.com>
2016-04-19 13:16:14 +0530

    Submit Commit

--- src/FixBST.cpp
+++ src/FixBST.cpp
@@ -31,7 +31,41 @@ struct node{
 	int data;
 	struct node *right;
 };
+int inorder_recc1(struct node*, struct node**, int);
 
 void fix_bst(struct node *root){
+	struct node *arr[100];
+	int len,i,j,swap;
+	if (root == NULL)
+	{
+		return;
+	}
+	len=inorder_recc1(root, arr, 0);
+	for (i = 0; i < len-1; i++)
+	{
+		if (arr[i]->data > arr[i + 1]->data)
+		{
+			break;
+		}
+	}
+	for (j = i + 1; j < len&&arr[j]->data < arr[i]->data; j++);
 
+	swap = arr[i]->data;
+	arr[i]->data = arr[j - 1]->data;
+	arr[j - 1]->data = arr[i]->data;
+}
+
+
+int inorder_recc1(struct node *root, struct node **arr, int i)
+{
+	if (root == NULL)
+	{
+		return i;
+	}
+
+	i = inorder_recc1(root->left, arr, i);
+	arr[i] = root;
+	i++;
+	i = inorder_recc1(root->right, arr, i);
+	return i;
 }
